5d960fed65a334fb3bab7a01c002e1137d3cc5d2
luanarmo <luisangel_arroyo@outlook.com>
2024-10-11 23:40:30 -0600

    feat: Modificar accion 'destroy' para implementar servicio de eliminacion

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,7 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Libro, Autor
 from .serializers import LibroSerializer, AutorSerializer
+from .services import eliminar_autor
 from util_commons.pagination import CustomPagination
 
 
@@ -15,3 +18,8 @@ class AutorViewSet(ModelViewSet):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializer
     pagination_class = CustomPagination
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        eliminar_autor(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)

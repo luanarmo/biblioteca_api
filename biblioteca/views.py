d2624a3a7cf804da476d1bba2d0766b92f473779
luanarmo <luisangel_arroyo@outlook.com>
2024-10-11 23:07:56 -0600

    feat: Definir ModelViewSets para los modelos Autor y Libro

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Libro, Autor
+from .serializers import LibroSerializer, AutorSerializer
+from ..util_commons.pagination import CustomPagination
+
+
+class LibroViewSet(ModelViewSet):
+    queryset = Libro.objects.all()
+    serializer_class = LibroSerializer
+    pagination_class = CustomPagination
+
+
+class AutorViewSet(ModelViewSet):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+    pagination_class = CustomPagination
